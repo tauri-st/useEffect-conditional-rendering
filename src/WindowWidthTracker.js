0861dca69eddb97be4f5c34083e59dbc47c3ca63
tauri_s <tauristclaire@gmail.com>
2024-01-04 12:01:32 -0800

    Add cleanup function to remove event listener when tracker is untoggled

--- src/WindowWidthTracker.js
+++ src/WindowWidthTracker.js
@@ -10,13 +10,18 @@ const WindowWidthTracker = () => {
       console.log(window.innerWidth);
     };
 
-    // the event listener on our window object creates a long running 
+    // 1. the event listener on our window object creates a long running 
     // process that is outside the scope of our WindowWidthTracker 
     // component. It will keep on running whether the WindowWidthTracker
     // component still exists or not. And with every ‘re’-toggling of 
     // the WindowWidthTracker, we create another resize event listener 
     // attached to the window object.
     window.addEventListener("resize", trackWindowWidth);
+
+    // 2. Add cleanup function to remove event listener
+    return () => {
+      window.removeEventListener("resize", trackWindowWidth);
+    };
   }, []);
 
   return <p>The window is now {windowWidth}px wide!</p>;
