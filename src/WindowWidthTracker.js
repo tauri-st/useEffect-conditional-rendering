2a94778caff1c0622ff9e198ff1b88eb7cbe1ef5
tauri_s <tauristclaire@gmail.com>
2024-01-04 11:47:00 -0800

    Console out innerwidth of window to view how component keeps running even when toggled off and add notes about malfunction

--- src/WindowWidthTracker.js
+++ src/WindowWidthTracker.js
@@ -7,8 +7,15 @@ const WindowWidthTracker = () => {
   useEffect(() => {
     const trackWindowWidth = () => {
       setWindowWidth(window.innerWidth);
+      console.log(window.innerWidth);
     };
 
+    // the event listener on our window object creates a long running 
+    // process that is outside the scope of our WindowWidthTracker 
+    // component. It will keep on running whether the WindowWidthTracker
+    // component still exists or not. And with every ‘re’-toggling of 
+    // the WindowWidthTracker, we create another resize event listener 
+    // attached to the window object.
     window.addEventListener("resize", trackWindowWidth);
   }, []);
 
